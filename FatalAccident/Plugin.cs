f46754cdbf55f91a7054bef4a7ade2871f5f8b0f
Denys Hromniuk <50991602+MrSydar@users.noreply.github.com>
2024-03-02 07:08:36 -0800

    Initial commit

---
+++ FatalAccident/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using UnityEngine.UIElements.UIR;
+
+namespace FatalAccident
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PLUGIN_GUID = "mrsydar.fatalaccident";
+        private const string PLUGIN_NAME = "fatal accident";
+        private const string PLUGIN_VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ FatalAccident/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FatalAccident
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "FatalAccident";
+        public const string PLUGIN_NAME = "FatalAccident";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
